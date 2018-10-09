78316945c1ac2d176ad0f25ce2c399cdff0895be
Matthew Wong <mawong@redhat.com>
2018-10-09 17:23:17 -0400

    Get platform/cloudprovider from cluster-config-v1 configmap

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -2,15 +2,16 @@ package stub
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/openshift/cluster-storage-operator/pkg/apis/storage/v1alpha1"
 
+	"github.com/openshift/installer/pkg/types"
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
-	"github.com/sirupsen/logrus"
+	//"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
+	utilyaml "k8s.io/apimachinery/pkg/util/yaml"
 )
 
 func NewHandler() sdk.Handler {
@@ -18,51 +19,60 @@ func NewHandler() sdk.Handler {
 }
 
 type Handler struct {
-	// Fill me
 }
 
 func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 	switch o := event.Object.(type) {
 	case *v1alpha1.ClusterStorage:
-		err := sdk.Create(newbusyBoxPod(o))
-		if err != nil && !errors.IsAlreadyExists(err) {
-			logrus.Errorf("failed to create busybox pod : %v", err)
-			return err
-		}
+		return h.sync(o)
 	}
 	return nil
 }
 
-// newbusyBoxPod demonstrates how to create a busybox pod
-func newbusyBoxPod(cr *v1alpha1.ClusterStorage) *corev1.Pod {
-	labels := map[string]string{
-		"app": "busy-box",
+func (h *Handler) sync(_ *v1alpha1.ClusterStorage) error {
+	platform, err := getPlatform()
+	if err != nil {
+		return err
 	}
-	return &corev1.Pod{
+	if platform.AWS != nil {
+		h.syncAWS(*platform.AWS)
+	} else if platform.Libvirt != nil {
+	} else if platform.OpenStack != nil {
+	}
+
+	return nil
+}
+
+func (h *Handler) syncAWS(types.AWSPlatform) error {
+
+	// TODO create default ebs storage class
+	// TODO efs operator installation (create subscription)
+	return nil
+}
+
+func getPlatform() (*types.Platform, error) {
+	cm := &corev1.ConfigMap{
 		TypeMeta: metav1.TypeMeta{
-			Kind:       "Pod",
+			Kind:       "ConfigMap",
 			APIVersion: "v1",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "busy-box",
-			Namespace: cr.Namespace,
-			OwnerReferences: []metav1.OwnerReference{
-				*metav1.NewControllerRef(cr, schema.GroupVersionKind{
-					Group:   v1alpha1.SchemeGroupVersion.Group,
-					Version: v1alpha1.SchemeGroupVersion.Version,
-					Kind:    "ClusterStorage",
-				}),
-			},
-			Labels: labels,
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "busybox",
-					Image:   "docker.io/busybox",
-					Command: []string{"sleep", "3600"},
-				},
-			},
+			Name:      "cluster-config-v1",
+			Namespace: "kube-system",
 		},
 	}
+	err := sdk.Get(cm)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := utilyaml.ToJSON([]byte(cm.Data["install-config"]))
+	if err != nil {
+		return nil, err
+	}
+
+	config := &types.InstallConfig{}
+	json.Unmarshal(data, &config)
+
+	return &config.Platform, nil
 }
