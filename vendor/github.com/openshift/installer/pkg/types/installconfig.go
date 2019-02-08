3d56dd41cdf699a160fff682d59f20a1f65620c0
Matthew Wong <mawong@redhat.com>
2019-02-08 10:42:56 -0500

    vendor

--- vendor/github.com/openshift/installer/pkg/types/installconfig.go
+++ vendor/github.com/openshift/installer/pkg/types/installconfig.go
@@ -1,19 +1,33 @@
 package types
 
 import (
-	"net"
-
 	"github.com/openshift/installer/pkg/ipnet"
+	"github.com/openshift/installer/pkg/types/aws"
+	"github.com/openshift/installer/pkg/types/libvirt"
+	"github.com/openshift/installer/pkg/types/none"
+	"github.com/openshift/installer/pkg/types/openstack"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
-	// PlatformNameAWS is name for AWS platform.
-	PlatformNameAWS string = "aws"
-	// PlatformNameOpenstack is name for Openstack platform.
-	PlatformNameOpenstack string = "openstack"
-	// PlatformNameLibvirt is name for Libvirt platform.
-	PlatformNameLibvirt string = "libvirt"
+	// InstallConfigVersion is the version supported by this package.
+	InstallConfigVersion = "v1beta2"
+)
+
+var (
+	// PlatformNames is a slice with all the visibly-supported
+	// platform names in alphabetical order. This is the list of
+	// platforms presented to the user in the interactive wizard.
+	PlatformNames = []string{
+		aws.Name,
+	}
+	// HiddenPlatformNames is a slice with all the
+	// hidden-but-supported platform names. This list isn't presented
+	// to the user in the interactive wizard.
+	HiddenPlatformNames = []string{
+		none.Name,
+		openstack.Name,
+	}
 )
 
 // InstallConfig is the configuration for an OpenShift install.
@@ -23,20 +37,21 @@ type InstallConfig struct {
 
 	metav1.ObjectMeta `json:"metadata"`
 
-	// ClusterID is the ID of the cluster.
-	ClusterID string `json:"clusterID"`
-
-	// Admin is the configuration for the admin user.
-	Admin Admin `json:"admin"`
+	// SSHKey is the public ssh key to provide access to instances.
+	// +optional
+	SSHKey string `json:"sshKey,omitempty"`
 
 	// BaseDomain is the base domain to which the cluster should belong.
 	BaseDomain string `json:"baseDomain"`
 
 	// Networking defines the pod network provider in the cluster.
-	Networking `json:"networking"`
+	*Networking `json:"networking,omitempty"`
 
 	// Machines is the list of MachinePools that need to be installed.
-	Machines []MachinePool `json:"machines"`
+	// +optional
+	// Default on AWS and OpenStack is 3 masters and 3 workers.
+	// Default on Libvirt is 1 master and 1 worker.
+	Machines []MachinePool `json:"machines,omitempty"`
 
 	// Platform is the configuration for the specific platform upon which to
 	// perform the installation.
@@ -57,27 +72,24 @@ func (c *InstallConfig) MasterCount() int {
 	return 1
 }
 
-// Admin is the configuration for the admin user.
-type Admin struct {
-	// Email is the email address of the admin user.
-	Email string `json:"email"`
-	// Password is the password of the admin user.
-	Password string `json:"password"`
-	// SSHKey to use for the access to compute instances.
-	SSHKey string `json:"sshKey,omitempty"`
-}
-
 // Platform is the configuration for the specific platform upon which to perform
 // the installation. Only one of the platform configuration should be set.
 type Platform struct {
 	// AWS is the configuration used when installing on AWS.
-	AWS *AWSPlatform `json:"aws,omitempty"`
+	// +optional
+	AWS *aws.Platform `json:"aws,omitempty"`
 
 	// Libvirt is the configuration used when installing on libvirt.
-	Libvirt *LibvirtPlatform `json:"libvirt,omitempty"`
+	// +optional
+	Libvirt *libvirt.Platform `json:"libvirt,omitempty"`
+
+	// None is the empty configuration used when installing on an unsupported
+	// platform.
+	None *none.Platform `json:"none,omitempty"`
 
 	// OpenStack is the configuration used when installing on OpenStack.
-	OpenStack *OpenStackPlatform `json:"openstack,omitempty"`
+	// +optional
+	OpenStack *openstack.Platform `json:"openstack,omitempty"`
 }
 
 // Name returns a string representation of the platform (e.g. "aws" if
@@ -88,112 +100,52 @@ func (p *Platform) Name() string {
 		return ""
 	}
 	if p.AWS != nil {
-		return PlatformNameAWS
+		return aws.Name
 	}
 	if p.Libvirt != nil {
-		return PlatformNameLibvirt
+		return libvirt.Name
+	}
+	if p.None != nil {
+		return none.Name
 	}
 	if p.OpenStack != nil {
-		return PlatformNameOpenstack
+		return openstack.Name
 	}
 	return ""
 }
 
 // Networking defines the pod network provider in the cluster.
 type Networking struct {
-	Type        NetworkType `json:"type"`
-	ServiceCIDR ipnet.IPNet `json:"serviceCIDR"`
-	PodCIDR     ipnet.IPNet `json:"podCIDR"`
-}
-
-// NetworkType defines the pod network provider in the cluster.
-type NetworkType string
-
-const (
-	// NetworkTypeOpenshiftSDN is used to install with SDN.
-	NetworkTypeOpenshiftSDN NetworkType = "openshift-sdn"
-	// NetworkTypeOpenshiftOVN is used to install with OVN.
-	NetworkTypeOpenshiftOVN NetworkType = "openshift-ovn"
-)
-
-// AWSPlatform stores all the global configuration that
-// all machinesets use.
-type AWSPlatform struct {
-	// Region specifies the AWS region where the cluster will be created.
-	Region string `json:"region"`
-
-	// UserTags specifies additional tags for AWS resources created for the cluster.
-	UserTags map[string]string `json:"userTags,omitempty"`
-
-	// DefaultMachinePlatform is the default configuration used when
-	// installing on AWS for machine pools which do not define their own
-	// platform configuration.
-	DefaultMachinePlatform *AWSMachinePoolPlatform `json:"defaultMachinePlatform,omitempty"`
-
-	// VPCID specifies the vpc to associate with the cluster.
-	// If empty, new vpc will be created.
+	// MachineCIDR is the IP address space from which to assign machine IPs.
 	// +optional
-	VPCID string `json:"vpcID"`
+	// Default is 10.0.0.0/16 for all platforms other than Libvirt.
+	// For Libvirt, the default is 192.168.126.0/24.
+	MachineCIDR *ipnet.IPNet `json:"machineCIDR,omitempty"`
 
-	// VPCCIDRBlock
+	// Type is the network type to install
 	// +optional
-	VPCCIDRBlock string `json:"vpcCIDRBlock"`
-}
+	// Default is OpenShiftSDN.
+	Type string `json:"type,omitempty"`
 
-// OpenStackPlatform stores all the global configuration that
-// all machinesets use.
-type OpenStackPlatform struct {
-	// Region specifies the OpenStack region where the cluster will be created.
-	Region string `json:"region"`
-
-	// DefaultMachinePlatform is the default configuration used when
-	// installing on OpenStack for machine pools which do not define their own
-	// platform configuration.
-	DefaultMachinePlatform *OpenStackMachinePoolPlatform `json:"defaultMachinePlatform,omitempty"`
-
-	// NetworkCIDRBlock
+	// ServiceCIDR is the IP address space from which to assign service IPs.
 	// +optional
-	NetworkCIDRBlock string `json:"NetworkCIDRBlock"`
+	// Default is 172.30.0.0/16.
+	ServiceCIDR *ipnet.IPNet `json:"serviceCIDR,omitempty"`
 
-	// BaseImage
-	// Name of image to use from OpenStack cloud
-	BaseImage string `json:"baseImage"`
-
-	// Cloud
-	// Name of OpenStack cloud to use from clouds.yaml
-	Cloud string `json:"cloud"`
-
-	// ExternalNetwork
-	// The OpenStack external network to be used for installation.
-	ExternalNetwork string `json:"externalNetwork"`
+	// ClusterNetworks is the IP address space from which to assign pod IPs.
+	// +optional
+	// Default is a single cluster network with a CIDR of 10.128.0.0/14
+	// and a host subnet length of 9.
+	ClusterNetworks []ClusterNetworkEntry `json:"clusterNetworks,omitempty"`
 }
 
-// LibvirtPlatform stores all the global configuration that
-// all machinesets use.
-type LibvirtPlatform struct {
-	// URI is the identifier for the libvirtd connection.  It must be
-	// reachable from both the host (where the installer is run) and the
-	// cluster (where the cluster-API controller pod will be running).
-	URI string `json:"URI"`
+// ClusterNetworkEntry is a single IP address block for pod IP blocks. IP blocks
+// are allocated with size 2^HostSubnetLength.
+type ClusterNetworkEntry struct {
+	// The IP block address pool
+	CIDR ipnet.IPNet `json:"cidr"`
 
-	// DefaultMachinePlatform is the default configuration used when
-	// installing on AWS for machine pools which do not define their own
-	// platform configuration.
-	DefaultMachinePlatform *LibvirtMachinePoolPlatform `json:"defaultMachinePlatform,omitempty"`
-
-	// Network
-	Network LibvirtNetwork `json:"network"`
-
-	// MasterIPs
-	MasterIPs []net.IP `json:"masterIPs"`
-}
-
-// LibvirtNetwork is the configuration of the libvirt network.
-type LibvirtNetwork struct {
-	// Name is the name of the nework.
-	Name string `json:"name"`
-	// IfName is the name of the network interface.
-	IfName string `json:"if"`
-	// IPRange is the range of IPs to use.
-	IPRange string `json:"ipRange"`
+	// The size of blocks to allocate from the larger pool.
+	// This is the length in bits - so a 9 here will allocate a /23.
+	HostSubnetLength int32 `json:"hostSubnetLength"`
 }
