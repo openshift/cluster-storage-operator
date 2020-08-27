f5a138b53a0bc1bb626a5058f1a38ca4cafaa8b2
Fabio Bertinatto <fbertina@redhat.com>
2020-08-27 16:23:42 +0200

    Manila and oVirt operators should not create credentials
    CredentialRequests objects have been moved to ./manifests dir,
    so they will be applied by CSO regardless of the cloud. Therefore,
    CSI operators should ot create the credentials themselves. This
    patch is a requirement to allow cloud-credentials-controller to
    run in Manual mode. More information at
    https://github.com/openshift/cloud-credential-operator/blob/master/docs/mode-manual-creds.md

--- pkg/operator/csidriveroperator/csioperatorclient/ovirt.go
+++ pkg/operator/csidriveroperator/csioperatorclient/ovirt.go
@@ -6,9 +6,6 @@ import (
 
 	configv1 "github.com/openshift/api/config/v1"
 	"github.com/openshift/cluster-storage-operator/pkg/csoclients"
-	"github.com/openshift/cluster-storage-operator/pkg/generated"
-	"github.com/openshift/library-go/pkg/controller/factory"
-	"github.com/openshift/library-go/pkg/operator/csi/credentialsrequestcontroller"
 	"github.com/openshift/library-go/pkg/operator/events"
 )
 
@@ -39,20 +36,6 @@ func GetOVirtCSIOperatorConfig(clients *csoclients.Clients, recorder events.Reco
 		CRAsset:         "csidriveroperators/ovirt/08_cr.yaml",
 		DeploymentAsset: "csidriveroperators/ovirt/07_deployment.yaml",
 		ImageReplacer:   strings.NewReplacer(pairs...),
-		ExtraControllers: []factory.Controller{
-			newOVirtCredentialsRequest(clients, recorder),
-		},
-		Optional: false,
+		Optional:        false,
 	}
-}
-
-func newOVirtCredentialsRequest(clients *csoclients.Clients, recorder events.Recorder) factory.Controller {
-	crController := credentialsrequestcontroller.NewCredentialsRequestController(
-		"oVirt",
-		csoclients.CSIOperatorNamespace,
-		generated.MustAsset("csidriveroperators/ovirt/09_credential_request.yaml"),
-		clients.DynamicClient,
-		clients.OperatorClient,
-		recorder)
-	return crController
 }
