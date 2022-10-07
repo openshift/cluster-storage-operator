498f84c8be593dc418a73e53e5950635b958846b
Hemant Kumar <hekumar@redhat.com>
2022-10-07 10:43:20 -0400

    Rename assets directories

--- pkg/operator/csidriveroperator/csioperatorclient/aws.go
+++ pkg/operator/csidriveroperator/csioperatorclient/aws.go
@@ -23,7 +23,6 @@ func GetAWSEBSCSIOperatorConfig(isHypershift bool) CSIOperatorConfig {
 		CSIDriverName:   AWSEBSCSIDriverName,
 		ConditionPrefix: "AWSEBS",
 		Platform:        configv1.AWSPlatformType,
-		CRAsset:         "csidriveroperators/aws-ebs/10_cr.yaml",
 		ImageReplacer:   strings.NewReplacer(pairs...),
 		AllowDisabled:   false,
 	}
@@ -38,21 +37,23 @@ func GetAWSEBSCSIOperatorConfig(isHypershift bool) CSIOperatorConfig {
 			"csidriveroperators/aws-ebs/standalone/07_role_aws_config.yaml",
 			"csidriveroperators/aws-ebs/standalone/08_rolebinding_aws_config.yaml",
 		}
+		csiDriverConfig.CRAsset = "csidriveroperators/aws-ebs/standalone/10_cr.yaml"
 		csiDriverConfig.DeploymentAsset = "csidriveroperators/aws-ebs/standalone/09_deployment.yaml"
 	} else {
 		csiDriverConfig.GuestStaticAssets = []string{
-			"csidriveroperators/aws-ebs/guest_assets/02_sa.yaml",
-			"csidriveroperators/aws-ebs/guest_assets/03_role.yaml",
-			"csidriveroperators/aws-ebs/guest_assets/04_rolebinding.yaml",
-			"csidriveroperators/aws-ebs/guest_assets/05_clusterrole.yaml",
-			"csidriveroperators/aws-ebs/guest_assets/06_clusterrolebinding.yaml",
+			"csidriveroperators/aws-ebs/hypershift/guest/02_sa.yaml",
+			"csidriveroperators/aws-ebs/hypershift/guest/03_role.yaml",
+			"csidriveroperators/aws-ebs/hypershift/guest/04_rolebinding.yaml",
+			"csidriveroperators/aws-ebs/hypershift/guest/05_clusterrole.yaml",
+			"csidriveroperators/aws-ebs/hypershift/guest/06_clusterrolebinding.yaml",
 		}
 		csiDriverConfig.MgmtStaticAssets = []string{
-			"csidriveroperators/aws-ebs/mgmt_assets/01_operator_role.yaml",
-			"csidriveroperators/aws-ebs/mgmt_assets/02_sa.yaml",
-			"csidriveroperators/aws-ebs/mgmt_assets/04_rolebinding.yaml",
+			"csidriveroperators/aws-ebs/hypershift/mgmt/01_operator_role.yaml",
+			"csidriveroperators/aws-ebs/hypershift/mgmt/02_sa.yaml",
+			"csidriveroperators/aws-ebs/hypershift/mgmt/04_rolebinding.yaml",
 		}
-		csiDriverConfig.DeploymentAsset = "csidriveroperators/aws-ebs/mgmt_assets/09_deployment.yaml"
+		csiDriverConfig.DeploymentAsset = "csidriveroperators/aws-ebs/hypershift/mgmt/09_deployment.yaml"
+		csiDriverConfig.CRAsset = "csidriveroperators/aws-ebs/hypershift/guest/10_cr.yaml"
 	}
 
 	return csiDriverConfig
