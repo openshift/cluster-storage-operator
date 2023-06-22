379bac28748f927e232b67a286e337864b18e39b
Hemant Kumar <hekumar@redhat.com>
2023-06-22 15:32:06 -0400

    Update generated manifests

--- hack/verify-manifest.sh
+++ hack/verify-manifest.sh
@@ -3,8 +3,8 @@
 ./hack/generate-manifests.sh
 
 if [[ -n $(git status -s assets/) ]]; then
-    echo 'ERROR: generated kustomize manifests needs to be updated. You can use make update to update them'
-    git diff
+    echo 'ERROR: generated kustomize manifests needs to be updated. You can use make update to update them.'
+    git --no-pager diff
     exit -1
 fi
 exit 0
