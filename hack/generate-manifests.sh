42cd288bfa17ba3870eaa8ddeecdf01e4c545604
Hemant Kumar <hekumar@redhat.com>
2023-11-17 16:33:10 -0500

    Add assets for azure-disk hypershift deployments

--- hack/generate-manifests.sh
+++ hack/generate-manifests.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-oc kustomize \
-    assets/csidriveroperators/aws-ebs/hypershift/guest \
-    -o assets/csidriveroperators/aws-ebs/hypershift/guest/generated
+drivers=( aws-ebs azure-disk )
 
-oc kustomize \
-    assets/csidriveroperators/aws-ebs/hypershift/mgmt \
-    -o assets/csidriveroperators/aws-ebs/hypershift/mgmt/generated
-    
-oc kustomize \
-    assets/csidriveroperators/aws-ebs/standalone \
-    -o assets/csidriveroperators/aws-ebs/standalone/generated
-    
+for driver in "${drivers[@]}"
+do
+    oc kustomize \
+        assets/csidriveroperators/${driver}/hypershift/guest \
+        -o assets/csidriveroperators/${driver}/hypershift/guest/generated
 
+    oc kustomize \
+        assets/csidriveroperators/${driver}/hypershift/mgmt \
+        -o assets/csidriveroperators/${driver}/hypershift/mgmt/generated
 
-
+    oc kustomize \
+        assets/csidriveroperators/${driver}/standalone \
+        -o assets/csidriveroperators/${driver}/standalone/generated
+done
 
