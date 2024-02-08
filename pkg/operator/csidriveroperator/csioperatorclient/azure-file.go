e5620fd6acc48a1e1007f4c7ee74853a17b711cd
Hemant Kumar <hekumar@redhat.com>
2024-02-08 15:16:59 -0500

    Patch for azure file control plane image

--- pkg/operator/csidriveroperator/csioperatorclient/azure-file.go
+++ pkg/operator/csidriveroperator/csioperatorclient/azure-file.go
@@ -8,9 +8,10 @@ import (
 )
 
 const (
-	AzureFileDriverName             = "file.csi.azure.com"
-	envAzureFileDriverOperatorImage = "AZURE_FILE_DRIVER_OPERATOR_IMAGE"
-	envAzureFileDriverImage         = "AZURE_FILE_DRIVER_IMAGE"
+	AzureFileDriverName                  = "file.csi.azure.com"
+	envAzureFileDriverOperatorImage      = "AZURE_FILE_DRIVER_OPERATOR_IMAGE"
+	envAzureFileDriverImage              = "AZURE_FILE_DRIVER_IMAGE"
+	envAzureFileDriverControlPlangeImage = "AZURE_FILE_DRIVER_CONTROL_PLANE_IMAGE"
 )
 
 func IsNotAzueStackCloud(status *configv1.InfrastructureStatus, isInstalled bool) bool {
@@ -26,29 +27,49 @@ func IsNotAzueStackCloud(status *configv1.InfrastructureStatus, isInstalled bool
 	return true
 }
 
-func GetAzureFileCSIOperatorConfig() CSIOperatorConfig {
+func GetAzureFileCSIOperatorConfig(isHyperShift bool) CSIOperatorConfig {
 	pairs := []string{
 		"${OPERATOR_IMAGE}", os.Getenv(envAzureFileDriverOperatorImage),
 		"${DRIVER_IMAGE}", os.Getenv(envAzureFileDriverImage),
 		"${CLUSTER_CLOUD_CONTROLLER_MANAGER_OPERATOR_IMAGE}", os.Getenv(envCCMOperatorImage),
 		"${OPERATOR_IMAGE_VERSION}", os.Getenv(envOperatorImageVersion),
+		"${DRIVER_CONTROL_PLANE_IMAGE}", os.Getenv(envAzureFileDriverControlPlangeImage),
 	}
 
-	return CSIOperatorConfig{
+	csiDriverConfig := CSIOperatorConfig{
 		CSIDriverName:   AzureFileDriverName,
 		ConditionPrefix: "AzureFile",
 		Platform:        configv1.AzurePlatformType,
 		StatusFilter:    IsNotAzueStackCloud,
-		StaticAssets: []string{
-			"csidriveroperators/azure-file/03_sa.yaml",
-			"csidriveroperators/azure-file/04_role.yaml",
-			"csidriveroperators/azure-file/05_rolebinding.yaml",
-			"csidriveroperators/azure-file/06_clusterrole.yaml",
-			"csidriveroperators/azure-file/07_clusterrolebinding.yaml",
-		},
-		CRAsset:         "csidriveroperators/azure-file/09_cr.yaml",
-		DeploymentAsset: "csidriveroperators/azure-file/08_deployment.yaml",
 		ImageReplacer:   strings.NewReplacer(pairs...),
 		AllowDisabled:   false,
 	}
+
+	if !isHyperShift {
+		csiDriverConfig.StaticAssets = []string{
+			"csidriveroperators/azure-file/standalone/generated/v1_serviceaccount_azure-file-csi-driver-operator.yaml",
+			"csidriveroperators/azure-file/standalone/generated/rbac.authorization.k8s.io_v1_role_azure-file-csi-driver-operator-role.yaml",
+			"csidriveroperators/azure-file/standalone/generated/rbac.authorization.k8s.io_v1_rolebinding_azure-file-csi-driver-operator-rolebinding.yaml",
+			"csidriveroperators/azure-file/standalone/generated/rbac.authorization.k8s.io_v1_clusterrole_azure-file-csi-driver-operator-clusterrole.yaml",
+			"csidriveroperators/azure-file/standalone/generated/rbac.authorization.k8s.io_v1_clusterrolebinding_azure-file-csi-driver-operator-clusterrolebinding.yaml",
+		}
+		csiDriverConfig.DeploymentAsset = "csidriveroperators/azure-file/standalone/generated/apps_v1_deployment_azure-file-csi-driver-operator.yaml"
+		csiDriverConfig.CRAsset = "csidriveroperators/azure-file/standalone/generated/operator.openshift.io_v1_clustercsidriver_file.csi.azure.com.yaml"
+	} else {
+		csiDriverConfig.StaticAssets = []string{
+			"csidriveroperators/azure-file/hypershift/guest/generated/rbac.authorization.k8s.io_v1_clusterrole_azure-file-csi-driver-operator-clusterrole.yaml",
+			"csidriveroperators/azure-file/hypershift/guest/generated/rbac.authorization.k8s.io_v1_clusterrolebinding_azure-file-csi-driver-operator-clusterrolebinding.yaml",
+			"csidriveroperators/azure-file/hypershift/guest/generated/rbac.authorization.k8s.io_v1_role_azure-file-csi-driver-operator-role.yaml",
+			"csidriveroperators/azure-file/hypershift/guest/generated/rbac.authorization.k8s.io_v1_rolebinding_azure-file-csi-driver-operator-rolebinding.yaml",
+			"csidriveroperators/azure-file/hypershift/guest/generated/v1_serviceaccount_azure-file-csi-driver-operator.yaml",
+		}
+		csiDriverConfig.MgmtStaticAssets = []string{
+			"csidriveroperators/azure-file/hypershift/mgmt/generated/rbac.authorization.k8s.io_v1_role_azure-file-csi-driver-operator-role.yaml",
+			"csidriveroperators/azure-file/hypershift/mgmt/generated/rbac.authorization.k8s.io_v1_rolebinding_azure-file-csi-driver-operator-rolebinding.yaml",
+			"csidriveroperators/azure-file/hypershift/mgmt/generated/v1_serviceaccount_azure-file-csi-driver-operator.yaml",
+		}
+		csiDriverConfig.DeploymentAsset = "csidriveroperators/azure-file/hypershift/mgmt/generated/apps_v1_deployment_azure-file-csi-driver-operator.yaml"
+		csiDriverConfig.CRAsset = "csidriveroperators/azure-file/hypershift/guest/generated/operator.openshift.io_v1_clustercsidriver_file.csi.azure.com.yaml"
+	}
+	return csiDriverConfig
 }
