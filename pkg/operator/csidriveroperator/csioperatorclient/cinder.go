341ce03f1c538f2c5905f69686052b19ebedf3b0
Maysa Macedo <maysa.macedo95@gmail.com>
2024-07-16 15:27:43 -0300

    Enable Cinder in Hypershift

--- pkg/operator/csidriveroperator/csioperatorclient/cinder.go
+++ pkg/operator/csidriveroperator/csioperatorclient/cinder.go
@@ -15,26 +15,47 @@ const (
 	envOpenStackCinderDriverImage         = "OPENSTACK_CINDER_DRIVER_IMAGE"
 )
 
-func GetOpenStackCinderCSIOperatorConfig(clients *csoclients.Clients, recorder events.Recorder) CSIOperatorConfig {
+func GetOpenStackCinderCSIOperatorConfig(clients *csoclients.Clients, recorder events.Recorder, isHypershift bool) CSIOperatorConfig {
 	pairs := []string{
 		"${OPERATOR_IMAGE}", os.Getenv(envOpenStackCinderDriverOperatorImage),
 		"${DRIVER_IMAGE}", os.Getenv(envOpenStackCinderDriverImage),
 	}
 
-	return CSIOperatorConfig{
+	csiDriverConfig := CSIOperatorConfig{
 		CSIDriverName:   OpenStackCinderDriverName,
 		ConditionPrefix: "OpenStackCinder",
 		Platform:        configv1.OpenStackPlatformType,
-		StaticAssets: []string{
-			"csidriveroperators/openstack-cinder/02_sa.yaml",
-			"csidriveroperators/openstack-cinder/03_role.yaml",
-			"csidriveroperators/openstack-cinder/04_rolebinding.yaml",
-			"csidriveroperators/openstack-cinder/05_clusterrole.yaml",
-			"csidriveroperators/openstack-cinder/06_clusterrolebinding.yaml",
-		},
-		CRAsset:         "csidriveroperators/openstack-cinder/08_cr.yaml",
-		DeploymentAsset: "csidriveroperators/openstack-cinder/07_deployment.yaml",
-		ImageReplacer:   strings.NewReplacer(pairs...),
-		AllowDisabled:   false,
+
+		ImageReplacer: strings.NewReplacer(pairs...),
+		AllowDisabled: false,
 	}
+
+	if !isHypershift {
+		csiDriverConfig.StaticAssets = []string{
+			"csidriveroperators/openstack-cinder/standalone/02_sa.yaml",
+			"csidriveroperators/openstack-cinder/standalone/03_role.yaml",
+			"csidriveroperators/openstack-cinder/standalone/04_rolebinding.yaml",
+			"csidriveroperators/openstack-cinder/standalone/05_clusterrole.yaml",
+			"csidriveroperators/openstack-cinder/standalone/06_clusterrolebinding.yaml",
+		}
+		csiDriverConfig.CRAsset = "csidriveroperators/openstack-cinder/08_cr.yaml"
+		csiDriverConfig.DeploymentAsset = "csidriveroperators/openstack-cinder/07_deployment.yaml"
+	} else {
+		csiDriverConfig.StaticAssets = []string{
+			"csidriveroperators/openstack-cinder/hypershift/guest/02_sa.yaml",
+			"csidriveroperators/openstack-cinder/hypershift/guest/03_role.yaml",
+			"csidriveroperators/openstack-cinder/hypershift/guest/04_rolebinding.yaml",
+			"csidriveroperators/openstack-cinder/hypershift/guest/05_clusterrole.yaml",
+			"csidriveroperators/openstack-cinder/hypershift/guest/06_clusterrolebinding.yaml",
+		}
+		csiDriverConfig.MgmtStaticAssets = []string{
+			"csidriveroperators/openstack-cinder/hypershift/mgmt/01_role.yaml",
+			"csidriveroperators/openstack-cinder/hypershift/mgmt/01_sa.yaml",
+			"csidriveroperators/openstack-cinder/hypershift/mgmt/04_rolebinding.yaml",
+		}
+		csiDriverConfig.DeploymentAsset = "csidriveroperators/openstack-cinder/hypershift/mgmt/07_deployment.yaml"
+		csiDriverConfig.CRAsset = "csidriveroperators/openstack-cinder/hypershift/guest/08_cr.yaml"
+	}
+
+	return csiDriverConfig
 }
