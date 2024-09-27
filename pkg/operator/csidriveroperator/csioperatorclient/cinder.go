2ac59d817340ce1c2c0420c1405ce02b52ab5f0a
Stephen Finucane <stephenfin@redhat.com>
2024-09-27 17:45:29 +0100

    openstack-cinder: Integrate kustomize
    This currently does zilch, but it establishes the framework we can use
    as we work on Hypershift integration.
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- pkg/operator/csidriveroperator/csioperatorclient/cinder.go
+++ pkg/operator/csidriveroperator/csioperatorclient/cinder.go
@@ -29,14 +29,14 @@ func GetOpenStackCinderCSIOperatorConfig(isHypershift bool) CSIOperatorConfig {
 
 	if !isHypershift {
 		csiDriverConfig.StaticAssets = []string{
-			"csidriveroperators/openstack-cinder/02_sa.yaml",
-			"csidriveroperators/openstack-cinder/03_role.yaml",
-			"csidriveroperators/openstack-cinder/04_rolebinding.yaml",
-			"csidriveroperators/openstack-cinder/05_clusterrole.yaml",
-			"csidriveroperators/openstack-cinder/06_clusterrolebinding.yaml",
+			"csidriveroperators/openstack-cinder/standalone/generated/v1_serviceaccount_openstack-cinder-csi-driver-operator.yaml",
+			"csidriveroperators/openstack-cinder/standalone/generated/rbac.authorization.k8s.io_v1_role_openstack-cinder-csi-driver-operator-role.yaml",
+			"csidriveroperators/openstack-cinder/standalone/generated/rbac.authorization.k8s.io_v1_rolebinding_openstack-cinder-csi-driver-operator-rolebinding.yaml",
+			"csidriveroperators/openstack-cinder/standalone/generated/rbac.authorization.k8s.io_v1_clusterrole_openstack-cinder-csi-driver-operator-clusterrole.yaml",
+			"csidriveroperators/openstack-cinder/standalone/generated/rbac.authorization.k8s.io_v1_clusterrolebinding_openstack-cinder-csi-driver-operator-clusterrolebinding.yaml",
 		}
-		csiDriverConfig.CRAsset = "csidriveroperators/openstack-cinder/08_cr.yaml"
-		csiDriverConfig.DeploymentAsset = "csidriveroperators/openstack-cinder/07_deployment.yaml"
+		csiDriverConfig.CRAsset = "csidriveroperators/openstack-cinder/standalone/generated/operator.openshift.io_v1_clustercsidriver_cinder.csi.openstack.org.yaml"
+		csiDriverConfig.DeploymentAsset = "csidriveroperators/openstack-cinder/standalone/generated/apps_v1_deployment_openstack-cinder-csi-driver-operator.yaml"
 	} else {
 		panic("Hypershift unsupported")
 	}
