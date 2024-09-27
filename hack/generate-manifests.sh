4ac624940f4b130195a59867bc3978f86606db65
Stephen Finucane <stephenfin@redhat.com>
2024-09-27 17:47:01 +0100

    hack: Address shellcheck issues
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- hack/generate-manifests.sh
+++ hack/generate-manifests.sh
@@ -2,18 +2,16 @@
 
 drivers=( aws-ebs azure-disk azure-file )
 
-for driver in "${drivers[@]}"
-do
+for driver in "${drivers[@]}"; do
     oc kustomize \
-        assets/csidriveroperators/${driver}/hypershift/guest \
-        -o assets/csidriveroperators/${driver}/hypershift/guest/generated
+        "assets/csidriveroperators/${driver}/hypershift/guest" \
+        -o "assets/csidriveroperators/${driver}/hypershift/guest/generated"
 
     oc kustomize \
-        assets/csidriveroperators/${driver}/hypershift/mgmt \
-        -o assets/csidriveroperators/${driver}/hypershift/mgmt/generated
+        "assets/csidriveroperators/${driver}/hypershift/mgmt" \
+        -o "assets/csidriveroperators/${driver}/hypershift/mgmt/generated"
 
     oc kustomize \
-        assets/csidriveroperators/${driver}/standalone \
-        -o assets/csidriveroperators/${driver}/standalone/generated
+        "assets/csidriveroperators/${driver}/standalone" \
+        -o "assets/csidriveroperators/${driver}/standalone/generated"
 done
-
