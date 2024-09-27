6ab668de7187074bf23c38c753c0bc9068eeb30a
Stephen Finucane <stephenfin@redhat.com>
2024-09-27 17:50:13 +0100

    hack: Make HyperShift asset generation optional
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- hack/generate-manifests.sh
+++ hack/generate-manifests.sh
@@ -3,13 +3,15 @@
 drivers=( aws-ebs azure-disk azure-file )
 
 for driver in "${drivers[@]}"; do
-    oc kustomize \
-        "assets/csidriveroperators/${driver}/hypershift/guest" \
-        -o "assets/csidriveroperators/${driver}/hypershift/guest/generated"
-
-    oc kustomize \
-        "assets/csidriveroperators/${driver}/hypershift/mgmt" \
-        -o "assets/csidriveroperators/${driver}/hypershift/mgmt/generated"
+    # Ignore drivers that don't (yet) support HyperShift
+    if [ -d "assets/csidriveroperators/${driver}/hypershift" ]; then
+        oc kustomize \
+            "assets/csidriveroperators/${driver}/hypershift/guest" \
+            -o "assets/csidriveroperators/${driver}/hypershift/guest/generated"
+        oc kustomize \
+            "assets/csidriveroperators/${driver}/hypershift/mgmt" \
+            -o "assets/csidriveroperators/${driver}/hypershift/mgmt/generated"
+    fi
 
     oc kustomize \
         "assets/csidriveroperators/${driver}/standalone" \
